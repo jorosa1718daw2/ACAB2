ef8234a23f6ab5b20b924170cc01a1fbb39cd820
SIGE <jorosa1718daw2@gmail.com>
2017-12-19 09:03:45 +0100

    jon snow

--- src/app/focus-contaminant/focus-contaminant.component.ts
+++ src/app/focus-contaminant/focus-contaminant.component.ts
@@ -2,47 +2,52 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http/src/response';
 
-
 //TODO: Crear vistas!
+
+enum StatusCode {};
+
+interface IdName {
+  id: number;
+  name: string;
+};
+
+interface MeasuringComponent extends IdName { };
+interface Unit extends IdName { };
+
+interface AnalogData {
+  timeStamp: string; // Esto se deberia transformar a Date o a moment (mirar locale/UTC...)
+  value: number;
+  sensorId: number;
+};
+
+interface Sensor {
+  id: number;
+  measuringComponent: MeasuringComponent;
+  unit: Unit;
+  unitId: number;
+  measuringComponentId: number;
+  analyzerId: number;
+  /* Al final estos 2 iran por separado */
+  //historicalAnalogData: Array<any>; // Por definir
+  //currentAnalogData: CurrentAnalogData;
+};
+
+interface Analyzer {
+  id: number;
+  manufacturer: string;
+  model: string;
+  serialNumber: number;
+  statusCode: StatusCode;
+  sensors: Array<Sensor>;
+  focusId: number;
+};
+
 interface Focus {
   id: number;
   name: string;
   description: string;
   analyzers: Array<Analyzer>;
-}
-
-interface Analyzer {
-  id: number;
-  focusId: number;
-  manufacturer: string;
-  model: string;
-  serialNumber: number;
-  sensors: Array<sensor>;
-}
-
-interface sensor {
-  id: number;
-  analyzerId: number;
-  measuringComponentId: string;
-  unitId: number;
-  measuringComponents: Array<measuringComponent>;
-}
-interface measuringComponent{
-  id: number;
-  name: string;
-}
-
-interface unit {
-  id: number;
-  name:string;
-}
-interface currentAnalogData {
-  id: number;
-  timeStamp: string;
-  value: number;
-  sensorId: number;
-}
-
+};
 
 @Component({
   selector: 'app-focus-contaminant',
@@ -51,15 +56,42 @@ interface currentAnalogData {
 })
 export class FocusContaminantComponent {
   focuscontaminant: Array<Focus>;
+  // cada uno de estos serviran como models para los distintos ngFor
+  focuses: Array<Focus>
+  selectedFocus: Focus;
+  selectedAnalyzer: Analyzer;
+  selectedSensor: Sensor;
+  measurements: Array<AnalogData>;
 
   constructor(private http: HttpClient) {
+    this.focuses = [];
+    this.selectedFocus = null;
+    this.selectedAnalyzer = null;
+    this.selectedSensor = null;
+    this.measurements = []; // aqui se mostraran los valores (historicos y tiempo real)
     this.getFocusContaminants();
   }
+
+  // Mirar como pasar la fecha: Date | moment.Moment | string
+  getSensorHistoricalValues(sensorId: number, dateIni: string, dateEnd: string)
+
+  // Este tambien devolvera un array, pero solo con 1 posicion
+  getSensorActualValue(sensorId: number)
+
+
   getFocusContaminants(): void {
-    this.http.get<Focus[]>('http://192.168.10.12:63390/api/focus')
+    this.http.get< Array<Focus> >('http://192.168.10.12:63390/api/focus')
       .subscribe(data => {
         console.log(data);
         this.focuscontaminant = data;
+        // Asignar valores directamente o iterar para verificar tipos, etc ¿?
+        this.focuses = Object.assign({},data); // Copia por valor - test
+        if (Array.isArray(this.focuses) && this.focuses.length) {
+          // Esta referenciado (a la hora de modificar, se hace copia por valor)
+          this.selectedFocus = this.focuses[0];
+        }
+        if (this.selectedFocus)     this.selectedAnalyzer = this.selectedFocus[0];
+        if (this.selectedAnalyzer)  this.selectedSensor = this.selectedAnalyzer[0];
 
       },
       (err: HttpErrorResponse) => {
