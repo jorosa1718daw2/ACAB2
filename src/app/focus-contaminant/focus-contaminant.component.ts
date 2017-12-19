c24aa1c85824a81ad92ec8c37ce47632ad493619
SIGE <jorosa1718daw2@gmail.com>
2017-12-19 12:41:48 +0100

    Focus tabla

--- src/app/focus-contaminant/focus-contaminant.component.ts
+++ src/app/focus-contaminant/focus-contaminant.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http/src/response';
+import moment = require('moment');
 
-//TODO: Crear vistas!
 
-enum StatusCode {};
+
+enum StatusCode { };
 
 interface IdName {
   id: number;
@@ -27,9 +28,6 @@ interface Sensor {
   unitId: number;
   measuringComponentId: number;
   analyzerId: number;
-  /* Al final estos 2 iran por separado */
-  //historicalAnalogData: Array<any>; // Por definir
-  //currentAnalogData: CurrentAnalogData;
 };
 
 interface Analyzer {
@@ -55,43 +53,56 @@ interface Focus {
   styleUrls: ['./focus-contaminant.component.css']
 })
 export class FocusContaminantComponent {
+
   focuscontaminant: Array<Focus>;
   // cada uno de estos serviran como models para los distintos ngFor
   focuses: Array<Focus>
-  selectedFocus: Focus;
-  selectedAnalyzer: Analyzer;
-  selectedSensor: Sensor;
-  measurements: Array<AnalogData>;
+  analyzers: Array<Analyzer>;
+  sensors: Array<Sensor>;
+  //measurements: Array<AnalogData>;
 
   constructor(private http: HttpClient) {
     this.focuses = [];
-    this.selectedFocus = null;
-    this.selectedAnalyzer = null;
-    this.selectedSensor = null;
-    this.measurements = []; // aqui se mostraran los valores (historicos y tiempo real)
+    //this.analyzers = [];
+    //this.sensors = [];
+    //this.measurements = []; // aqui se mostraran los valores (historicos y tiempo real)
     this.getFocusContaminants();
+    //this.getAnalyzers();
   }
+  
 
   // Mirar como pasar la fecha: Date | moment.Moment | string
-  getSensorHistoricalValues(sensorId: number, dateIni: string, dateEnd: string)
+  getSensorHistoricalValues(sensorId: number, dateIni: moment.Moment, dateEnd: moment.Moment): void {
+    let dInicial: string = dateIni.format('YYYY-MM-DD_HH:mm');
+    let dFinal: string = dateEnd.format('YYYY-MM-DD_HH:mm');
+    this.http.get<Array<AnalogData>>('http://192.168.10.12:63390/api/sensor/:id/historicalanalogdata')
+      .subscribe(data => {
+
+      });
+
+  }
 
   // Este tambien devolvera un array, pero solo con 1 posicion
-  getSensorActualValue(sensorId: number)
+  getSensorActualValue(sensorId: number): void {
+    this.http.get<Array<AnalogData>>('http://192.168.10.12:63390/api/sensor/:id/currentanalogdata')
+    .subscribe(data => {
+        //this.measurements = data;
+    });
+  }
+
 
 
   getFocusContaminants(): void {
-    this.http.get< Array<Focus> >('http://192.168.10.12:63390/api/focus')
+    this.http.get<Array<Focus>>('http://192.168.10.12:63390/api/focus')
       .subscribe(data => {
         console.log(data);
         this.focuscontaminant = data;
-        // Asignar valores directamente o iterar para verificar tipos, etc ¿?
-        this.focuses = Object.assign({},data); // Copia por valor - test
-        if (Array.isArray(this.focuses) && this.focuses.length) {
-          // Esta referenciado (a la hora de modificar, se hace copia por valor)
-          this.selectedFocus = this.focuses[0];
+        this.focuses = data.slice(0); // copia x valor
+        /*if (Array.isArray(this.focuses) && this.focuses.length) {
+          this.analyzers
         }
-        if (this.selectedFocus)     this.selectedAnalyzer = this.selectedFocus[0];
-        if (this.selectedAnalyzer)  this.selectedSensor = this.selectedAnalyzer[0];
+        if (this.selectedFocus) this.selectedAnalyzer = this.selectedFocus[0];
+        if (this.selectedAnalyzer) this.selectedSensor = this.selectedAnalyzer[0];*/
 
       },
       (err: HttpErrorResponse) => {
@@ -102,5 +113,20 @@ export class FocusContaminantComponent {
         }
       });
   }
+
+
+  /*getAnalyzers(): void {
+    this.http.get<Array<Analyzer>>('http://192.168.10.12:63390/api/analyzer').subscribe(data => {
+        this.analyzers = data;
+
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log("Client-Side Error Ocurred")
+        } else {
+          console.log("Server-side Error Ocurred")
+        }
+      });
+  }*/
 
 }
